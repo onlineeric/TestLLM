4c963a2219b5490f01d1125927d1c5f294ad85e9
Eric Cheng <online.eric@gmail.com>
2024-05-09 16:47:24 +1200

    update llama3 use CPU

--- learning-topics/test-Meta-Llama-3-8B.py
+++ learning-topics/test-Meta-Llama-3-8B.py
@@ -3,16 +3,21 @@ from transformers import pipeline
 
 hf_token = os.getenv('HUGGINGFACE_TOKEN')
 
+start_time = time.time()
 generator = pipeline('text-generation', model='meta-llama/Meta-Llama-3-8B', 
                      token=hf_token, 
-                     #device=0,
-										 model_kwargs={
-											"torch_dtype": torch.float16,
-											"quantization_config": {"load_in_4bit": True},
-											"low_cpu_mem_usage": True,
+                     device=-1,
+										model_kwargs={
+										# "torch_dtype": torch.float16,
+										# 	"quantization_config": {"load_in_4bit": True},
+										"low_cpu_mem_usage": True,
     								})
+
+end_time = time.time()
+print("Time taken for pipeline: ", end_time - start_time, "seconds")
+
 start_time = time.time()
-res = generator('The color of an apple is', max_length=100, num_return_sequences=2)
+res = generator('The color of an apple is', max_length=100, num_return_sequences=3)
 end_time = time.time()
 print("Time taken: ", end_time - start_time, "seconds")
 print(json.dumps(res, indent=4))
