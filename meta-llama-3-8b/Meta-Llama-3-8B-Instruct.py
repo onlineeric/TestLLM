a074fbab83041e7fff2263bd3d8ed59c24db3464
Eric Cheng <online.eric@gmail.com>
2024-05-14 23:24:18 +1200

    testing to use more prompts and more tokens

--- meta-llama-3-8b/Meta-Llama-3-8B-Instruct.py
+++ meta-llama-3-8b/Meta-Llama-3-8B-Instruct.py
@@ -6,7 +6,13 @@ hf_token = os.getenv('HUGGINGFACE_TOKEN')
 
 messages = [
 		{"role": "system", "content": "You are a chatbot."},
-		{"role": "user", "content": "How to train my dog to sit?"},
+		{"role": "user", "content": "Tell me how to train my dog to sit?"},
+		{"role": "system", "content": "To train your dog to sit, follow these steps:"},
+		{"role": "system", "content": "1. Hold a treat close to your dog's nose."},
+		{"role": "system", "content": "2. Move your hand up, allowing your dog's head to follow the treat and causing their bottom to lower."},
+		{"role": "system", "content": "3. Once they are in sitting position, say 'sit' and give them the treat."},
+		{"role": "system", "content": "4. Repeat this process several times a day until your dog has mastered the command."},
+		{"role": "user", "content": "My dog is not responding to the 'sit' command. What should I do?"},
 ]
 
 pipeline = transformers.pipeline(
