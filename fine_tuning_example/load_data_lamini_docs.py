aa035e8a8a0515e4f04d55c93511744bae76808d
Eric Cheng <online.eric@gmail.com>
2024-05-24 16:47:36 +1200

    added prompt template to load lamini doc

--- fine_tuning_example/load_data_lamini_docs.py
+++ fine_tuning_example/load_data_lamini_docs.py
@@ -5,8 +5,21 @@ from datasets import load_dataset
 pretrained_dataset = load_dataset("lamini/lamini_docs", split="train", streaming=True)
 
 n = 10
-print("Pretrained dataset:")
 top_n = itertools.islice(pretrained_dataset, n)
+# for i in top_n:
+#   print("$$$ Question: ", i["question"])
+#   print("$$$ Answer: ", i["answer"])
+
+prompt_template_qa = """
+### Question:
+{question}
+
+### Answer:
+{answer}
+"""
+
+print("Pretrained dataset:")
 for i in top_n:
-  print("$$$ Question: ", i["question"])
-  print("$$$ Answer: ", i["answer"])
+  text_with_prompt_template = prompt_template_qa.format(question=i["question"], answer=i["answer"])
+  print(text_with_prompt_template)
+  
