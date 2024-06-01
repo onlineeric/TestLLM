9d54792ed491e33984375cd1526c598aa911234e
Eric Cheng <online.eric@gmail.com>
2024-06-01 20:55:22 +1200

    update fine tuning

--- fine_tuning_example/fine_tuning_cooking.py
+++ fine_tuning_example/fine_tuning_cooking.py
@@ -1,21 +1,29 @@
 from datasets import load_dataset
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling, EarlyStoppingCallback
 
 # Load the pre-trained model and tokenizer
-model_id = "EleutherAI/pythia-70m"
+#trained_model_name = "pythia-70m-finetuned-cooking_recipes"
+trained_model_name = "pythia-410m-finetuned-cooking_recipes"
+output_dir = f"gitignore_trained_models/{trained_model_name}"
+#model_id = "EleutherAI/pythia-70m"
+model_id = "EleutherAI/pythia-410m"
 model = AutoModelForCausalLM.from_pretrained(model_id)
-model.to("cuda")
+model.to('cuda')
 tokenizer = AutoTokenizer.from_pretrained(model_id)
 tokenizer.pad_token = tokenizer.eos_token
 
 # Load the dataset from the local directory
-dataset = load_dataset("./gitignore_datasets/cooking_recipes")
-train_dataset = dataset["train"].select(range(4000))
-eval_dataset = dataset["train"].select(range(4000, 5000))
+dataset = load_dataset("./gitignore_datasets/cooking_recipes", split='train[:500]')
+train_dataset = dataset.select(range(400))
+eval_dataset = dataset.select(range(400, 500))
+print("$$$ load dataset done")
 
 # Tokenize the dataset
 def tokenize_function(examples):
-	return tokenizer(examples["directions"], return_tensors="np", truncation=True, max_length=512)
+	return tokenizer(examples["directions"], 
+				#return_tensors="np", 
+				truncation=True, 
+				max_length=512)
 
 tokenized_train_datasets = train_dataset.map(tokenize_function, batched=True, remove_columns=["title", "ingredients", "NER", "link", "source"])
 tokenized_eval_datasets = eval_dataset.map(tokenize_function, batched=True, remove_columns=["title", "ingredients", "NER", "link", "source"])
@@ -26,9 +34,6 @@ data_collator = DataCollatorForLanguageModeling(
 	mlm=False,  # Set to True if you are doing masked language modeling
 )
 
-trained_model_name = "pythia-70m-finetuned-cooking_recipes"
-output_dir = f"gitignore_trained_models/{trained_model_name}"
-
 # Set up the training arguments
 training_args = TrainingArguments(
 	output_dir=output_dir,
@@ -36,12 +41,12 @@ training_args = TrainingArguments(
 	#eval_strategy="no",
 	save_strategy="epoch",
 	learning_rate=2e-5,
-	per_device_train_batch_size=8,
-	per_device_eval_batch_size=8,
+	per_device_train_batch_size=4,
+	per_device_eval_batch_size=4,
 	num_train_epochs=3,
 	weight_decay=0.01,
 	logging_steps=500,
-	load_best_model_at_end=True,
+	load_best_model_at_end=True,	
 )
 
 # Create the Trainer object
@@ -52,12 +57,11 @@ trainer = Trainer(
 	eval_dataset=tokenized_eval_datasets,
 	tokenizer=tokenizer,
 	data_collator=data_collator,
+	callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]  # Add early stopping
 )
 
-print("$$$$$$ Fine-tuning the model started...")
 # Fine-tune the model
 trainer.train()
-print("$$$$$$ Fine-tuning the model ended")
 
 # Save the fine-tuned model
 trainer.save_model(f"{output_dir}/final")
--- fine_tuning_example/load_local_data_model_and_compare.py
+++ fine_tuning_example/load_local_data_model_and_compare.py
@@ -1,8 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from inference import inference
 
-model_id = "EleutherAI/pythia-70m"
-finetuned_model_dir = "./gitignore_trained_models/pythia-70m-finetuned-cooking_recipes/final"
+model_id = "EleutherAI/pythia-410m"
+finetuned_model_dir = "./gitignore_trained_models/pythia-410m-finetuned-cooking_recipes/final"
 
 finetuned_slightly_model = AutoModelForCausalLM.from_pretrained(finetuned_model_dir, local_files_only=True)
 finetuned_slightly_model.to('cuda')
@@ -13,7 +13,7 @@ hf_model.to('cuda')
 hf_tokenizer = AutoTokenizer.from_pretrained(model_id)
 
 test_dataset = [
-	"Boil and debone chicken.",
+	"Wash potatoes; prick several times with a fork. Microwave them with a wet paper towel covering the potatoes on high for 6-8 minutes.",
 	"In a slow cooker, combine all ingredients. Cover and cook on low for 4 hours or until heated through and cheese is melted. Stir well before serving. Yields 6 servings.",
 	"Boil and debone chicken.", "Put bite size pieces in average size square casserole dish.", "Pour gravy and cream of mushroom soup over chicken; level.", "Make stuffing according to instructions on box (do not make too moist).", "Put stuffing on top of chicken and gravy; level.", "Sprinkle shredded cheese on top and bake at 350\u00b0 for approximately 20 minutes or until golden and bubbly.",
 	"Wash potatoes; prick several times with a fork.", "Microwave them with a wet paper towel covering the potatoes on high for 6-8 minutes.", "The potatoes should be soft, ready to eat.", "Let them cool enough to handle.", "Cut in half lengthwise; scoop out pulp and reserve.", "Discard shells.", "Brown ground beef until done.", "Drain any grease from the meat.", "Set aside when done.", "Meat will be added later.", "Melt butter in a large kettle over low heat; add flour, stirring until smooth.", "Cook 1 minute, stirring constantly. Gradually add milk; cook over medium heat, stirring constantly, until thickened and bubbly.", "Stir in potato, ground beef, salt, pepper, 1 cup of cheese, 2 tablespoons of green onion and 1/2 cup of bacon.", "Cook until heated (do not boil).", "Stir in sour cream if desired; cook until heated (do not boil).", "Sprinkle with remaining cheese, bacon and green onions.",
