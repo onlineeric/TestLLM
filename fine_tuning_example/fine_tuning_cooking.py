3fa4250a89ed2081877b485596390960f66bb789
Eric Cheng <online.eric@gmail.com>
2024-06-01 13:16:04 +1200

    updated fine tuning cooking

--- fine_tuning_example/fine_tuning_cooking.py
+++ fine_tuning_example/fine_tuning_cooking.py
@@ -1,49 +1,27 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
 from datasets import load_dataset
-import torch
-import time
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer
 
-# Load the dataset
-dataset = load_dataset('CodeKapital/CookingRecipes')
+# Load the pre-trained model and tokenizer
+model_id = "EleutherAI/pythia-70m"
+model = AutoModelForCausalLM.from_pretrained(model_id)
+model.to("cuda")
+tokenizer = AutoTokenizer.from_pretrained(model_id)
 
-# Select only the first 1000 samples
-dataset = dataset['train'].select(range(100))
 
-# Load the tokenizer and model
-model_name = "EleutherAI/pythia-70m"
-#model_name = "EleutherAI/pythia-410m"
-tokenizer = AutoTokenizer.from_pretrained(model_name)
-model = AutoModelForCausalLM.from_pretrained(model_name)
+
+# Load the dataset from the local directory
+dataset = load_dataset("./gitignore_datasets/cooking_recipes", split="train")
 
 # Tokenize the dataset
-# def tokenize_function(examples):
-# 	# return tokenizer(examples['title'] + " " + examples['ingredients'] + " " + examples['directions'], padding="max_length", truncation=True, max_length=512)
-# 	# Ensure all necessary keys are present and not empty
-# 	title = examples.get('title', "")
-# 	print('$$$ title: ', title)
-# 	ingredients = examples.get('ingredients', "")
-# 	directions = examples.get('directions', "")
-	
-# 	# Combine the text fields
-# 	text = title + " " + ingredients + " " + directions
-	
-# 	# Tokenize the combined text
-# 	return tokenizer(text, padding="max_length", truncation=True, max_length=512)
+def tokenize_function(examples):
+	return tokenizer(examples["directions"], truncation=True, padding="max_length", max_length=512)
 
-# tokenized_datasets = dataset.map(tokenize_function, batched=True)
+tokenized_datasets = dataset.map(tokenize_function, batched=True, num_proc=4)
 
-# Prepare for training
-# train_dataset = tokenized_datasets["train"]
-train_dataset = dataset['title', 'ingredients', 'directions', 'NER']
-
-# Check if a GPU is available and set the device accordingly
-device = "cuda" if torch.cuda.is_available() else "cpu"
-model.to(device)
-
-trained_model_name = f"CookingRecipes_{time.strftime('%Y%m%d_%H%M')}"
+trained_model_name = "pythia-70m-finetuned-cooking_recipes"
 output_dir = f"gitignore_trained_models/{trained_model_name}"
 
-# Define the training arguments
+# Set up the training arguments
 training_args = TrainingArguments(
 	output_dir=output_dir,
 	evaluation_strategy="epoch",
@@ -52,20 +30,19 @@ training_args = TrainingArguments(
 	per_device_eval_batch_size=8,
 	num_train_epochs=3,
 	weight_decay=0.01,
+	logging_steps=500,
 )
 
-# Define the Trainer
+# Create the Trainer object
 trainer = Trainer(
 	model=model,
 	args=training_args,
-	train_dataset=train_dataset,
+	train_dataset=tokenized_datasets["train"],
+	eval_dataset=tokenized_datasets["validation"],
 )
 
-# Train the model
+# Fine-tune the model
 trainer.train()
 
-# Save the model
-# model.save_pretrained("./gitignore_trained_models")
-# tokenizer.save_pretrained("./gitignore_trained_models")
-save_dir = f'{output_dir}/final'
-trainer.save_model(save_dir)
+# Save the fine-tuned model
+trainer.save_model(f"{output_dir}/final")
