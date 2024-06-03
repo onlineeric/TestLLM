43439c84b4bf51dd8c5e63762e4b05db44272eb4
Eric Cheng <online.eric@gmail.com>
2024-06-03 17:19:20 +1200

    create fine tuning cook files with new formatting prompt for fine tuning

---
+++ fine_tuning_cook/formatting_prompt_ft.py
@@ -0,0 +1,94 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, AutoModelForCausalLM, TrainingArguments, Trainer, DataCollatorForLanguageModeling, EarlyStoppingCallback
+import os
+
+model_name = "pythia-160m"	# "pythia-70m", "pythia-160m", "pythia-410m"
+model_id = f"EleutherAI/{model_name}"
+trained_model_name = f"{model_name}_ft_cooking"
+output_dir = f"gitignore_trained_models/{trained_model_name}"
+
+hf_token = os.getenv('HUGGINGFACE_TOKEN')
+
+# Load the pre-trained model and tokenizer
+model = AutoModelForCausalLM.from_pretrained(model_id, token=hf_token)
+tokenizer = AutoTokenizer.from_pretrained(model_id, token=hf_token)
+tokenizer.pad_token = tokenizer.eos_token
+
+# Load the dataset from the local directory
+dataset = load_dataset("../gitignore_datasets/cooking_recipes", split='train[:10]')
+train_dataset = dataset.select(range(8))
+eval_dataset = dataset.select(range(8, 10))
+print("\n$$$ load dataset done\n")
+
+# Tokenize the dataset
+def formatting_prompts_func(examples):
+	alpaca_prompt = """
+	### Instruction:
+	{}
+
+	### Input:
+	{}
+
+	### Response:
+	{}"""
+
+	response_template = """	Ingredients: {}
+	Steps: {}"""
+
+	titles			= examples["title"]
+	ingredients		= examples["ingredients"]
+	directions		= examples["directions"]
+	texts = []
+	for title, ingredient, direction in zip(titles, ingredients, directions):
+		# Must add EOS_TOKEN (tokenizer.eos_token), otherwise your generation will go on forever!
+		text = alpaca_prompt.format(
+			"Tell me how to make this dish. Please include the ingredients and the steps to make it.",
+			title,
+			response_template.format(ingredient, direction)
+			) + tokenizer.eos_token
+		texts.append(text)
+	return tokenizer(texts, truncation=True, padding="max_length", max_length=512)
+
+tokenized_train_datasets = train_dataset.map(formatting_prompts_func, batched=True)
+tokenized_eval_datasets = eval_dataset.map(formatting_prompts_func, batched=True)
+print("\n$$$ tokenize datasets done\n")
+
+# Set up the data collator
+data_collator = DataCollatorForLanguageModeling(
+	tokenizer=tokenizer,
+	mlm=False,  # Set to True if you are doing masked language modeling
+)
+
+# Set up the training arguments
+training_args = TrainingArguments(
+	output_dir=output_dir,
+	eval_strategy="epoch",
+	#eval_strategy="no",
+	save_strategy="epoch",
+	learning_rate=2e-5,
+	per_device_train_batch_size=4,
+	per_device_eval_batch_size=4,
+	#gradient_accumulation_steps=2,  # Simulate a larger batch size
+	num_train_epochs=3,
+	weight_decay=0.01,
+	logging_steps=500,
+	load_best_model_at_end=True,
+	#fp16=True,  # Enable mixed precision training
+)
+
+# Create the Trainer object
+trainer = Trainer(
+	model=model,
+	args=training_args,
+	train_dataset=tokenized_train_datasets,
+	eval_dataset=tokenized_eval_datasets,
+	tokenizer=tokenizer,
+	data_collator=data_collator,
+	callbacks=[EarlyStoppingCallback(early_stopping_patience=3)]  # Add early stopping
+)
+
+# Fine-tune the model
+trainer.train()
+
+# Save the fine-tuned model
+trainer.save_model(f"{output_dir}/final")
---
+++ fine_tuning_cook/test_ft_data_model.py
@@ -0,0 +1,27 @@
+from transformers import AutoTokenizer, AutoModelForCausalLM
+from inference import inference
+from datasets import load_dataset
+
+model_name = "pythia-160m"	# "pythia-70m", "pythia-160m", "pythia-410m"
+model_id = f"EleutherAI/{model_name}"
+trained_model_name = f"{model_name}_ft_cooking"
+ft_model_dir = f"gitignore_trained_models/{trained_model_name}/final"
+
+finetuned_slightly_model = AutoModelForCausalLM.from_pretrained(ft_model_dir, local_files_only=True)
+finetuned_slightly_model.to('cuda')
+finetuned_slightly_tokenizer = AutoTokenizer.from_pretrained(ft_model_dir, local_files_only=True)
+
+hf_model = AutoModelForCausalLM.from_pretrained(model_id, device_map='cuda')
+hf_tokenizer = AutoTokenizer.from_pretrained(model_id, device_map='cuda')
+
+test_dataset = load_dataset("../gitignore_datasets/cooking_recipes", split='train[:5]')
+
+for i in range(5):
+	test_question = f"How to make {test_dataset[i]["title"]}?"
+	print("\n$$$ Question input (test):", test_question)
+
+	print("$$$ Hugging Face model's answer: ")
+	print(inference(test_question, hf_model, hf_tokenizer, 1000, 512))
+ 
+	print("$$$ Finetuned slightly model's answer: ")
+	print(inference(test_question, finetuned_slightly_model, finetuned_slightly_tokenizer, 1000, 512))
