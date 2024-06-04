fc5091fd0f21acb9cc3533d5a60cf3b141cf888d
Eric Cheng <online.eric@gmail.com>
2024-06-04 22:37:36 +1200

    updated testing script

--- fine_tuning_cook/test_ft_data_model.py
+++ fine_tuning_cook/test_ft_data_model.py
@@ -16,12 +16,22 @@ hf_tokenizer = AutoTokenizer.from_pretrained(model_id, device_map='cuda')
 
 test_dataset = load_dataset("../gitignore_datasets/cooking_recipes", split='train[:5]')
 
-for i in range(5):
-	test_question = f"How to make {test_dataset[i]["title"]}?"
-	print("\n$$$ Question input (test):", test_question)
+# for i in range(5):
+# 	test_question = f"How to make {test_dataset[i]["title"]}?"
+# 	print("\n$$$ Question input (test):", test_question)
 
-	print("$$$ Hugging Face model's answer: ")
-	print(inference(test_question, hf_model, hf_tokenizer, 1000, 512))
+# 	print("$$$ Hugging Face model's answer: ")
+# 	print(inference(test_question, hf_model, hf_tokenizer, 1000, 512))
  
+# 	print("$$$ Finetuned slightly model's answer: ")
+# 	print(inference(test_question, finetuned_slightly_model, finetuned_slightly_tokenizer, 1000, 512))
+
+# create a loop, input text from console, until I press ctrl+c
+while True:
+	user_input = input("Enter a question: ")
+	if not user_input:
+		continue
+	print("\n$$$ Hugging Face model's answer: ")
+	print(inference(user_input, hf_model, hf_tokenizer, 1000, 512))
 	print("$$$ Finetuned slightly model's answer: ")
-	print(inference(test_question, finetuned_slightly_model, finetuned_slightly_tokenizer, 1000, 512))
+	print(inference(user_input, finetuned_slightly_model, finetuned_slightly_tokenizer, 1000, 512))
